2bcdc4164216932e6fcca1de734b569ea07fee8d
Roberto <robhhr@outlook.com>
2019-06-14 03:06:35 -0700

    Create project folders and components

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+const gulp = require("gulp"),
+    sourcemaps = require('gulp-sourcemaps');
+    prettyError = require('gulp-prettyerror');
+    autoprefixer = require('gulp-autoprefixer');
+    terser = require("gulp-terser"),
+    rename = require("gulp-rename"),
+    browserSync = require('browser-sync').create(),
+    eslint = require("gulp-eslint");
+    sass = require('gulp-sass');
+    uglifycss = require('gulp-uglifycss');
+
+gulp.task('sass', function(done) {
+    gulp
+        .src('./src/sass/style.scss')
+        .pipe(sourcemaps.init())
+        .pipe(prettyError())
+        .pipe(sass())
+        .pipe(
+        autoprefixer({
+            browsers: ['last 2 versions']
+        })
+        )
+        .pipe(uglifycss())
+        .pipe(rename('style.min.css'))
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(gulp.dest('./build/css'));
+      
+        done();
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task("scripts", function() {
+    return gulp
+      .src('./src/js/*.js')
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failAfterError())
+      .pipe(terser())
+      .pipe(rename({ extname: ".min.js" }))
+      .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task("reload", function() {
+    browserSync.reload();
+});
+  
+gulp.task('watch', function(done) {
+    gulp.watch(["./src/js/*.js", "index.html"], gulp.series("scripts", "reload"));
+    gulp.watch('./src/sass/*.scss', gulp.series('sass'));
+    done();
+});
+  
+  
+gulp.task("default", gulp.parallel("watch", "browser-sync"));
